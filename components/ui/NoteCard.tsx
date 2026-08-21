668bede4d1764cb097135bf46bfde42758ce175a
nathfavour <116535483+nathfavour@users.noreply.github.com>
2026-08-21 13:51:17 +0000

    fix(notes): pass note.title ?? undefined to ShareLockButton

--- components/ui/NoteCard.tsx
+++ components/ui/NoteCard.tsx
@@ -440,7 +440,7 @@ const NoteCard: React.FC<NoteCardProps> = React.memo(({ note, onUpdate, onDelete
                   resourceId={note.$id}
                   isPublic={getNotePublicState(liveNote)}
                   isGuest={!!liveNote.isGuest}
-                  resourceTitle={note.title}
+                  resourceTitle={note.title ?? undefined}
                   dek={note.dek}
                   accentColor="#EC4899"
                   onPublished={({ isPublic, isGuest }) => {
