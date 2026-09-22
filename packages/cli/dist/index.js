9737669a3b185a6d21e56876b4a52e41b99f3086
nathfavour <116535483+nathfavour@users.noreply.github.com>
2026-09-22 02:47:35 +0000

    chore(cli): bump @kylrix/cli version to 1.0.2

--- packages/cli/dist/index.js
+++ packages/cli/dist/index.js
@@ -3687,7 +3687,7 @@ import * as os5 from "os";
 import { spawn } from "child_process";
 import pc19 from "picocolors";
 var PACKAGE_NAME = "@kylrix/cli";
-var CURRENT_VERSION = "1.0.1";
+var CURRENT_VERSION = "1.0.2";
 var CACHE_DIR = path5.join(os5.homedir(), ".kylrix");
 var CACHE_FILE = path5.join(CACHE_DIR, "update-cache.json");
 var CHECK_INTERVAL_MS = 12 * 60 * 60 * 1e3;
